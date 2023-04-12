75692d6e04188c1f44f2685304cbaf4fe06652ab
Philip Zingmark <philip.zingmark@gmail.com>
2023-04-12 12:57:43 +0200

    variable size and function to get valid spawn positions

--- source/main.c
+++ source/main.c
@@ -12,6 +12,7 @@ void updateScreen(SDL_Renderer *pRenderer, Player player, Tile map[], SDL_Textur
 int handleInput(Player *pPlayer, Tile map[], int movementAmount, int *pCharge);
 void movePlayer(Player *pPlayer, char direction);
 int checkCollision(Player player, Tile map[], char direction);
+SDL_Rect findEmptyTile(Tile map[]);
 
 int main(int argv, char **args)
 {
@@ -19,15 +20,30 @@ int main(int argv, char **args)
     int previousTime = 0, movementPreviousTime = 0, charge = 0;
     Player *pPlayer;
     Tile map[MAPSIZE * MAPSIZE];
+    int windowWidth = DEFAULT_WIDTH, windowHeight = DEFAULT_HEIGHT;
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_DisplayMode dm;
+
+    if (SDL_GetDesktopDisplayMode(0, &dm) != 0)
+    {
+        printf("SDL_GetDesktopDisplayMode failed: %s", SDL_GetError());
+    }
+    else
+    {
+        windowWidth = dm.h - 100, windowHeight = dm.h - 100;
+    }
+
+    int tileSize = (windowHeight / MAPSIZE);
 
     char fileName[31];
     do
     {
         printf("map name: ");
         scanf(" %30s", fileName);
-    } while (initMap(map, fileName) == -1);
+    } while (initMap(map, fileName, tileSize) == -1);
 
-    int windowWidth = DEFAULT_WIDTH, windowHeight = DEFAULT_HEIGHT;
+    // int windowWidth = dm.w, windowHeight = dm.h; //DEFAULT_WIDTH, windowHeight = DEFAULT_HEIGHT;
     SDL_Window *pWindow = SDL_CreateWindow(WINDOW_NAME, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, 0);
     if (!pWindow)
     {
@@ -67,12 +83,12 @@ int main(int argv, char **args)
     }
 
     SDL_QueryTexture(pTexturePlayer, NULL, NULL, &pPlayer->rect.w, &pPlayer->rect.h);
+    SDL_Rect spawnTile = findEmptyTile(map);
+    pPlayer->rect.x = spawnTile.x; // windowWidth / 2;
+    pPlayer->rect.y = spawnTile.y; // windowHeight / 2;
 
-    pPlayer->rect.x = windowWidth / 2;
-    pPlayer->rect.y = windowHeight / 2;
-
-    pPlayer->rect.w = TILESIZE;
-    pPlayer->rect.h = TILESIZE;
+    pPlayer->rect.w = map[0].wall.w;
+    pPlayer->rect.h = map[0].wall.w;
 
     while (run)
     {
@@ -101,6 +117,7 @@ int main(int argv, char **args)
     SDL_DestroyTexture(pTextureTiles[1]);
     SDL_DestroyTexture(pTextureTiles[2]);
     SDL_DestroyTexture(pTextureTiles[3]);
+    SDL_DestroyTexture(pTexturePlayer);
 
     SDL_DestroyRenderer(pRenderer);
     SDL_DestroyWindow(pWindow);
@@ -244,32 +261,42 @@ int checkCollision(Player player, Tile map[], char direction)
     switch (direction)
     {
     case 'W':
-        if (map[(((player.rect.y - 1) / TILESIZE) * MAPSIZE) + (player.rect.x / TILESIZE)].type)
-            return map[(((player.rect.y - 1) / TILESIZE) * MAPSIZE) + (player.rect.x / TILESIZE)].type;
-        else if (map[(((player.rect.y - 1) / TILESIZE) * MAPSIZE) + ((player.rect.x + (TILESIZE - 1)) / TILESIZE)].type)
-            return map[(((player.rect.y - 1) / TILESIZE) * MAPSIZE) + ((player.rect.x + (TILESIZE - 1)) / TILESIZE)].type;
+        if (map[(((player.rect.y - 1) / map[0].wall.w) * MAPSIZE) + (player.rect.x / map[0].wall.w)].type)
+            return map[(((player.rect.y - 1) / map[0].wall.w) * MAPSIZE) + (player.rect.x / map[0].wall.w)].type;
+        else if (map[(((player.rect.y - 1) / map[0].wall.w) * MAPSIZE) + ((player.rect.x + (map[0].wall.w - 1)) / map[0].wall.w)].type)
+            return map[(((player.rect.y - 1) / map[0].wall.w) * MAPSIZE) + ((player.rect.x + (map[0].wall.w - 1)) / map[0].wall.w)].type;
         break;
     case 'A':
-        if (map[(((player.rect.y) / TILESIZE) * MAPSIZE) + ((player.rect.x - 1) / TILESIZE)].type)
-            return map[(((player.rect.y) / TILESIZE) * MAPSIZE) + ((player.rect.x - 1) / TILESIZE)].type;
-        else if (map[(((player.rect.y + (TILESIZE - 1)) / TILESIZE) * MAPSIZE) + ((player.rect.x - 1) / TILESIZE)].type)
-            return map[(((player.rect.y + (TILESIZE - 1)) / TILESIZE) * MAPSIZE) + ((player.rect.x - 1) / TILESIZE)].type;
+        if (map[(((player.rect.y) / map[0].wall.w) * MAPSIZE) + ((player.rect.x - 1) / map[0].wall.w)].type)
+            return map[(((player.rect.y) / map[0].wall.w) * MAPSIZE) + ((player.rect.x - 1) / map[0].wall.w)].type;
+        else if (map[(((player.rect.y + (map[0].wall.w - 1)) / map[0].wall.w) * MAPSIZE) + ((player.rect.x - 1) / map[0].wall.w)].type)
+            return map[(((player.rect.y + (map[0].wall.w - 1)) / map[0].wall.w) * MAPSIZE) + ((player.rect.x - 1) / map[0].wall.w)].type;
         break;
     case 'S':
-        if (map[(((player.rect.y + TILESIZE) / TILESIZE) * MAPSIZE) + (player.rect.x / TILESIZE)].type)
-            return map[(((player.rect.y + TILESIZE) / TILESIZE) * MAPSIZE) + (player.rect.x / TILESIZE)].type;
-        else if (map[(((player.rect.y + TILESIZE) / TILESIZE) * MAPSIZE) + ((player.rect.x + (TILESIZE - 1)) / TILESIZE)].type)
-            return map[(((player.rect.y + TILESIZE) / TILESIZE) * MAPSIZE) + ((player.rect.x + (TILESIZE - 1)) / TILESIZE)].type;
+        if (map[(((player.rect.y + map[0].wall.w) / map[0].wall.w) * MAPSIZE) + (player.rect.x / map[0].wall.w)].type)
+            return map[(((player.rect.y + map[0].wall.w) / map[0].wall.w) * MAPSIZE) + (player.rect.x / map[0].wall.w)].type;
+        else if (map[(((player.rect.y + map[0].wall.w) / map[0].wall.w) * MAPSIZE) + ((player.rect.x + (map[0].wall.w - 1)) / map[0].wall.w)].type)
+            return map[(((player.rect.y + map[0].wall.w) / map[0].wall.w) * MAPSIZE) + ((player.rect.x + (map[0].wall.w - 1)) / map[0].wall.w)].type;
         break;
     case 'D':
-        if (map[(((player.rect.y) / TILESIZE) * MAPSIZE) + ((player.rect.x + TILESIZE) / TILESIZE)].type)
-            return map[(((player.rect.y) / TILESIZE) * MAPSIZE) + ((player.rect.x + TILESIZE) / TILESIZE)].type;
-        else if (map[(((player.rect.y + (TILESIZE - 1)) / TILESIZE) * MAPSIZE) + ((player.rect.x + TILESIZE) / TILESIZE)].type)
-            return map[(((player.rect.y + (TILESIZE - 1)) / TILESIZE) * MAPSIZE) + ((player.rect.x + TILESIZE) / TILESIZE)].type;
+        if (map[(((player.rect.y) / map[0].wall.w) * MAPSIZE) + ((player.rect.x + map[0].wall.w) / map[0].wall.w)].type)
+            return map[(((player.rect.y) / map[0].wall.w) * MAPSIZE) + ((player.rect.x + map[0].wall.w) / map[0].wall.w)].type;
+        else if (map[(((player.rect.y + (map[0].wall.w - 1)) / map[0].wall.w) * MAPSIZE) + ((player.rect.x + map[0].wall.w) / map[0].wall.w)].type)
+            return map[(((player.rect.y + (map[0].wall.w - 1)) / map[0].wall.w) * MAPSIZE) + ((player.rect.x + map[0].wall.w) / map[0].wall.w)].type;
         break;
     default:
         break;
     }
 
     return 0;
+}
+SDL_Rect findEmptyTile(Tile map[])
+{
+    for (int i = 0; i < MAPSIZE * MAPSIZE; i++)
+    {
+        if (!map[i].type)
+        {
+            return map[i].wall;
+        }
+    }
 }
