6828c17a0cb4efe88bc0b9ebf280e91469e1565f
Philip Zingmark <philip.zingmark@gmail.com>
2023-05-15 11:49:26 +0200

    removed unused menu functions

--- include/menu.h
+++ include/menu.h
@@ -2,8 +2,6 @@
 #ifndef menu_h
 #define menu_h
 
-int menu(Game *pGame);
-int mapSelection(Game *pGame);
 int joinServerMenu(Game *pGame);
 int getStringFromUser(char text[], SDL_Event event);
 int testSelectMenu(Game *pGame, char *mapName);
